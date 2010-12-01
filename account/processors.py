323c6249241b4cca8b2acbfd96be59d7b90b7c8b
Leander Hanwald <shockflash@web.de>
2010-12-01 07:57:37 +0100

    Test for an facebook object adding to context

--- account/processors.py
+++ account/processors.py
@@ -2,5 +2,20 @@ from django.contrib.auth.forms import AuthenticationForm
 
 def login(request):
     return {
+        # make Loginform available on all pages
         'loginform': AuthenticationForm(),
+    }
+
+
+def login(request):
+
+    fb = None
+    try:
+        fb = request.facebook2
+    except:
+        pass
+
+    return {
+        # make facebook user object available on all pages
+        'facebook': fb,
     }
